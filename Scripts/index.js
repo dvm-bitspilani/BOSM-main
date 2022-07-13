b0105432d784a98d4031bfb9c92cdb729173c9af
Prateek Kashyap <94394757+bit-by-bits@users.noreply.github.com>
2022-07-13 09:36:35 +0530

    timer animation

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,22 +1,44 @@
-const dDay = new Date("October 15, 2022 23:59:59").getTime();
+const bosm = new Date("October 15, 2022 23:59:59").getTime();
+let prev_days, prev_hrs, prev_min, days, hrs, min;
 
-function setTime() {
-    const timeNow = new Date().getTime();
+let count = setInterval(() => {
+  const current_time = new Date().getTime();
 
-    let days = Math.floor((dDay - timeNow) / (1000 * 60 * 60 * 24));
-    days = days < 10  ? `0${days}` : days
-    let hrs = Math.floor((dDay - timeNow) / (1000 * 60 * 60)) - days * 24;
-    hrs = hrs < 10 ? `0${hrs}` : hrs
-    let min = Math.floor((dDay - timeNow) / (1000 * 60)) - days * 24 * 60 - hrs * 60;
-    min = min < 10 ? `0${min}` : min
-    if (days < 0) {
-        days = '00';
-        hrs = '00';
-        min = '00';
-    }
-    console.log(days)
-    document.getElementById("days").innerHTML = days;
-    document.getElementById("hours").innerHTML = hrs;
-    document.getElementById("mins").innerHTML = min;
-}
-setInterval(setTime,60)
+  // updating number of days left
+  days = Math.floor((bosm - current_time) / (1000 * 60 * 60 * 24));
+  days = days < 10 ? `0${days}` : days;
+
+  // updating number of hours left (in that day)
+  hrs = Math.floor((bosm - current_time) / (1000 * 60 * 60)) - days * 24;
+  hrs = hrs < 10 ? `0${hrs}` : hrs;
+
+  // updating number of minutes left (in that hour)
+  min =
+    Math.floor((bosm - current_time) / (1000 * 60)) - days * 24 * 60 - hrs * 60;
+  min = min < 10 ? `0${min}` : min;
+
+  // when timer is completed (BOSM arrives)
+  if (days < 0) {
+    days = "00";
+    hrs = "00";
+    min = "00";
+  }
+
+  // adding animation to timer cards
+  document.getElementById("days").style.animation =
+    prev_days != days ? "card-flip 0.6s" : "none";
+  document.getElementById("hours").style.animation =
+    prev_hrs != hrs ? "card-flip 0.6s" : "none";
+  document.getElementById("mins").style.animation =
+    prev_min != min ? "card-flip 0.6s" : "none";
+
+  // updating the temporary variables
+  prev_days = days;
+  prev_hrs = hrs;
+  prev_min = min;
+
+  // updating the divs
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hrs;
+  document.getElementById("mins").innerHTML = min;
+}, 2000);
