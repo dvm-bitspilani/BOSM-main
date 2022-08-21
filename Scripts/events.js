79499ffb4aec9febbcad48c8ad5d9d0cb6b1edf9
jay-goyal <goyal.jay2003@gmail.com>
2022-08-22 00:17:00 +0530

    Revert to old events animation

--- Scripts/events.js
+++ Scripts/events.js
@@ -34,7 +34,6 @@ let evtActive = 0;
 let evtElems = [];
 let evtDots = [];
 let i = 0;
-let lastIdx = 0;
 
 const initEvtElems = () => {
   evtElems = [];
@@ -45,6 +44,8 @@ const initEvtElems = () => {
     evtElem.classList.add("evt");
     evtElem.id = i.toString();
     evtElem.style.backgroundImage = `url("Assets/sports/${name}.png")`;
+    let evtElemCont = document.createElement("div");
+    evtElemCont.classList.add("evt-item-cont");
     i++;
     let evtImg = document.createElement("img");
     evtImg.classList.add("evt-logo");
@@ -61,6 +62,7 @@ const initEvtElems = () => {
     evtElems.push(evtElem);
   }
 
+  console.log(evtElems);
   let dotsCount = evtElems.length / lengths;
 
   for (let i = 0; i < dotsCount; i++) {
@@ -70,19 +72,11 @@ const initEvtElems = () => {
     dot.addEventListener("click", () => {
       evtActive = i * lengths;
       setActive();
-      evtActive += lengths;
-      if (evtActive >= evtElems.length) {
-        evtActive = 0;
-      } else if (evtActive < 0) {
-        evtActive = evtElems.length - lengths;
-      }
       clearInterval(eventInterval);
       eventInterval = setInterval(appendActive, 3000);
     });
   }
-  for (let i = 1; i <= lengths; i++) {
-    evtElems[evtElems.length - i].classList.add("evt-active");
-  }
+  evtsCont.replaceChildren(...evtElems);
   evtsDotsCont.replaceChildren(...evtDots);
   setActive();
 };
@@ -91,97 +85,33 @@ const setActive = () => {
   evtElems.forEach((elem, idx) => {
     let dot = evtDots[Math.floor(idx / lengths)];
     let id = parseInt(elem.id);
+    if (elem.classList.contains("evt-active")) {
+      elem.classList.remove("evt-active");
+    }
     if (elem.classList.contains("evt-inactive")) {
       elem.classList.remove("evt-inactive");
     }
-    if (elem.classList.contains("evt-next")) {
-      elem.classList.remove("evt-next");
-    }
-    if (elem.classList.contains("evt-prev")) {
-      elem.classList.remove("evt-prev");
-    }
     if (dot.classList.contains("evt-dot-active")) {
       dot.classList.remove("evt-dot-active");
-    }
-    if (elem.classList.contains("evt-active")) {
-      elem.classList.remove("evt-active");
-      elem.classList.add("evt-prev");
-      elem.classList.add("evt-inactive");
     }
     if (id >= evtActive && id <= evtActive + lengths - 1) {
       elem.classList.add("evt-active");
-    } else if (id >= evtActive + lengths && id <= evtActive + 2 * lengths - 1) {
-      elem.classList.add("evt-next");
-      elem.classList.add("evt-inactive");
     } else {
       elem.classList.add("evt-inactive");
     }
-    let evtDiff = evtElems.length - evtActive;
-    if (evtDiff <= lengths) {
-      if (idx < evtDiff - lengths) {
-        elem.classList.add("evt-active");
-      } else if (idx < evtDiff - lengths + lengths) {
-        elem.classList.add("evt-next");
-      }
-    }
-  });
-  evtsCont.replaceChildren(
-    ...evtElems.filter((elem) => elem.classList.contains("evt-prev")),
-    ...evtElems.filter((elem) => elem.classList.contains("evt-active")),
-    ...evtElems.filter((elem) => elem.classList.contains("evt-next"))
-  );
-  evtDots[Math.floor(evtActive / lengths)].classList.add("evt-dot-active");
-};
-
-const setActiveMob = () => {
-  evtElems.forEach((elem, idx) => {
-    let dot = evtDots[Math.floor(idx / lengths)];
-    if (elem.classList.contains("evt-active")) {
-      elem.classList.remove("evt-active");
-      elem.classList.add("evt-inactive");
-      elem.classList.add("evt-prev");
-    } else if (elem.classList.contains("evt-next")) {
-      elem.classList.remove("evt-next");
-      elem.classList.remove("evt-inactive");
-      elem.classList.add("evt-active");
-    } else if (elem.classList.contains("evt-prev")) {
-      elem.classList.remove("evt-prev");
+    if (amountDisplay !== 0) {
+      elem.style.transform = `translateX(calc(-${evtActive} * (var(--evtLogoSize) + 3 * var(--evtPadding) + 2 * var(--evtMargin))))`;
     } else {
-      elem.className = "evt evt-inactive";
-    }
-    if (dot.classList.contains("evt-dot-active")) {
-      dot.classList.remove("evt-dot-active");
+      elem.style.transform = `translateX(calc(-${
+        evtActive / 4
+      } * (2 * var(--evtSize) + 4 * var(--evtMargin))))`;
     }
   });
-  let j = 0;
-  for (let i = 1; i <= 4; i++) {
-    if (lastIdx + i < evtElems.length) {
-      evtElems[lastIdx + i].classList.add("evt-next");
-      j++;
-    }
-  }
-  if (j < 4) {
-    for (let i = 0; i < lengths - j; i++) {
-      evtElems[i].classList.add("evt-next");
-      lastIdx = i;
-    }
-  } else {
-    lastIdx += 4;
-  }
-  evtsCont.replaceChildren(
-    ...evtElems.filter((elem) => elem.classList.contains("evt-prev")),
-    ...evtElems.filter((elem) => elem.classList.contains("evt-active")),
-    ...evtElems.filter((elem) => elem.classList.contains("evt-next"))
-  );
   evtDots[Math.floor(evtActive / lengths)].classList.add("evt-dot-active");
 };
 
-const appendActive = () => {
-  if (amountDisplay === 0) {
-    setActiveMob();
-  } else {
-    setActive();
-  }
+let appendActive = () => {
+  setActive();
   evtActive += lengths;
   if (evtActive >= evtElems.length) {
     evtActive = 0;
@@ -225,5 +155,5 @@ evtArrRight.addEventListener("click", () => {
   eventInterval = setInterval(appendActive, 3000);
 });
 
-// let eventInterval = setInterval(appendActive, 3000);
+let eventInterval = setInterval(appendActive, 3000);
 initEvtElems();
