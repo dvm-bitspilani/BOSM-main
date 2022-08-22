1c79bdcc86d34837595dd02970c9a785cc37ade6
CoderVaibhavS <vaibhavsingla123.vs@gmail.com>
2022-08-22 19:21:36 +0530

    style: made tablet view of gallery

--- Scripts/gallery.js
+++ Scripts/gallery.js
@@ -1,10 +1,16 @@
 let rightArr = document.querySelector('#right-arr');
 let leftArr = document.querySelector('#left-arr');
 let gall = document.querySelector('.gallery-grid');
+let disc1 = document.querySelector('#disc-1');
+let disc2 = document.querySelector('#disc-2');
+let touchstartX = 0;
+let touchendX = 0;
 
 let scrObj = {
     scr: 0,
-    scrollVal: `0px`
+    scrollVal: `0px`,
+    swiped: 0,
+    swipeCount: 0
 };
 
 const slideLeft = (scrObj) => {
@@ -25,3 +31,46 @@ const slideRight = (scrObj) => {
 
 leftArr.addEventListener('click', () => slideLeft(scrObj));
 rightArr.addEventListener('click', () => slideRight(scrObj));
+
+const swipe = (scrObj) => {
+    console.log('swipe')
+    if(touchstartX > touchendX) { 
+        gall.style.transform = `translateX(calc((-1)*.98%*(${scrObj.swipeCount}+1) + (-1*(50%+10px))))`;
+        scrObj.swipeCount += 1;
+        console.log('swipe right');
+    }
+    if(touchstartX < touchendX) {
+        gall.style.transform = `translateX(calc((-1)*.98%*(${scrObj.swipeCount}-1) + (-1*(50%+10px))))`;
+        scrObj.swipeCount -= 1;
+        console.log('swipe left');
+    }
+
+    if(scrObj.swipeCount % 2 === 0) {
+        disc2.classList.remove('active-disc');
+        disc1.classList.add('active-disc');
+    }
+
+    else {
+        disc1.classList.remove('active-disc');
+        disc2.classList.add('active-disc');
+    }
+}
+
+gall.addEventListener('touchstart', (event) => {
+    touchstartX = event.changedTouches[0].screenX;
+});
+
+gall.addEventListener('touchend', (event) => {
+    touchendX = event.changedTouches[0].screenX;
+    swipe(scrObj)
+    // scrObj.swiped = !(scrObj.swiped);
+});
+
+for(let i=1; i<=50; i++) {
+    for(let j=1; j<=12; j++) {
+        gall.innerHTML += 
+            `<div class="gallery-images" id=${j}>
+            <img src="../Assets/Gallery/${j}.jpg" alt="img">
+            </div>`;
+    }
+}
