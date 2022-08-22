ed491896790515c2ffe56b4948e82636eb3a4fd6
CoderVaibhavS <vaibhavsingla123.vs@gmail.com>
2022-08-22 13:40:32 +0530

    feat: gallery scroll

--- Scripts/gallery.js
+++ Scripts/gallery.js
@@ -2,13 +2,26 @@ let rightArr = document.querySelector('#right-arr');
 let leftArr = document.querySelector('#left-arr');
 let gall = document.querySelector('.gallery-grid');
 
-const slideLeft = () => {
-gall.style.transform = 'translateX(0)';
+let scrObj = {
+    scr: 0,
+    scrollVal: `0px`
+};
+
+const slideLeft = (scrObj) => {
+    if(scrObj.scr > 0) {
+        gall.style.transform = `translateX(calc(-1*${scrObj.scrollVal} + 10%))`;
+        scrObj.scr -= 10;
+        scrObj.scrollVal = `calc(${scrObj.scr}%)`;
+    }
 }
 
-const slideRight = () => {
-gall.style.transform = 'translateX(calc((-1)*((100% - 140px)/2 + 80px)))';
+const slideRight = (scrObj) => {
+    if(scrObj.scr < 50) {
+        gall.style.transform = `translateX(calc(-1*${scrObj.scrollVal} - 10%))`;
+        scrObj.scr += 10;
+        scrObj.scrollVal = `calc(${scrObj.scr}%)`;
+    }
 }
 
-leftArr.addEventListener('click', () => slideLeft());
-rightArr.addEventListener('click', () => slideRight());
+leftArr.addEventListener('click', () => slideLeft(scrObj));
+rightArr.addEventListener('click', () => slideRight(scrObj));
