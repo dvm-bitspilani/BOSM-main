47b57b767a6744393f41a2faae6e5b8bb3cafe1b
CoderVaibhavS <vaibhavsingla123.vs@gmail.com>
2022-08-22 20:13:53 +0530

    style: made gallery mobile view

--- Scripts/gallery.js
+++ Scripts/gallery.js
@@ -3,13 +3,15 @@ let leftArr = document.querySelector('#left-arr');
 let gall = document.querySelector('.gallery-grid');
 let disc1 = document.querySelector('#disc-1');
 let disc2 = document.querySelector('#disc-2');
+let resp = window.matchMedia("(max-width: 600px)");
 let touchstartX = 0;
 let touchendX = 0;
+
+let trans = (resp.matches) ? .99 : .98
 
 let scrObj = {
     scr: 0,
     scrollVal: `0px`,
-    swiped: 0,
     swipeCount: 0
 };
 
@@ -35,14 +37,12 @@ rightArr.addEventListener('click', () => slideRight(scrObj));
 const swipe = (scrObj) => {
     console.log('swipe')
     if(touchstartX > touchendX) { 
-        gall.style.transform = `translateX(calc((-1)*.98%*(${scrObj.swipeCount}+1) + (-1*(50%+10px))))`;
+        gall.style.transform = `translateX(calc((-1)*${trans}%*(${scrObj.swipeCount}+1) + (-1*(50%+10px))))`;
         scrObj.swipeCount += 1;
-        console.log('swipe right');
     }
     if(touchstartX < touchendX) {
-        gall.style.transform = `translateX(calc((-1)*.98%*(${scrObj.swipeCount}-1) + (-1*(50%+10px))))`;
+        gall.style.transform = `translateX(calc((-1)*${trans}%*(${scrObj.swipeCount}-1) + (-1*(50%+10px))))`;
         scrObj.swipeCount -= 1;
-        console.log('swipe left');
     }
 
     if(scrObj.swipeCount % 2 === 0) {
@@ -62,8 +62,7 @@ gall.addEventListener('touchstart', (event) => {
 
 gall.addEventListener('touchend', (event) => {
     touchendX = event.changedTouches[0].screenX;
-    swipe(scrObj)
-    // scrObj.swiped = !(scrObj.swiped);
+    swipe(scrObj);
 });
 
 for(let i=1; i<=50; i++) {
