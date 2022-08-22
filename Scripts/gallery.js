219de9017b5d05236ae77e61ae97cb72cfaafe04
CoderVaibhavS <vaibhavsingla123.vs@gmail.com>
2022-08-22 11:19:14 +0530

    made gallery

---
+++ Scripts/gallery.js
@@ -0,0 +1,14 @@
+let rightArr = document.querySelector('#right-arr');
+let leftArr = document.querySelector('#left-arr');
+let gall = document.querySelector('.gallery-grid');
+
+const slideLeft = () => {
+gall.style.transform = 'translateX(0)';
+}
+
+const slideRight = () => {
+gall.style.transform = 'translateX(calc((-1)*((100% - 120px)/7*3 + 60px)))';
+}
+
+leftArr.addEventListener('click', () => slideLeft());
+rightArr.addEventListener('click', () => slideRight());
